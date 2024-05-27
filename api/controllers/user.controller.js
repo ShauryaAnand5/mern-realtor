0d43848e4417464ce140d7c92cecf635dc9c5359
ShauryaA <shauryaanand9941@gmail.com>
2024-05-27 19:11:16 +0530

    Created a Test API Route.

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test = async (req, res) => {
+    res.send({
+        message: 'API route is working.',
+    })
+}
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import connectToMongoDB from "./db/connectionToDB.js";
+import userRouter from './routes/user.route.js'
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -12,10 +13,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
 });
 
 
-
 const app = express();
 
 app.listen(3000, () => {
     connectToMongoDB();
   console.log("The server is running on port 3000!!");
 });
+
+app.use('/api/user', userRouter)
