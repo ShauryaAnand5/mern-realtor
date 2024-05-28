55dc56097b15d0249e12bffefc9e00e3e5c200c5
ShauryaA <shauryaanand9941@gmail.com>
2024-05-28 16:26:48 +0530

    Completed the Sign In page, and did some cosmetic changes in the Sign Up page.

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 export const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form className='flex flex-col gap-2'>
+        <input type="email" placeholder='Email' className="border p-3 rounded-lg" id="email"/>
+        <input type="password" placeholder='Password' className="border p-3 rounded-lg" id="password"/>
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign In</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>{"Don't have an account?"}</p>
+        <span className="text-blue-700">
+          <Link to="/sign-up">Sign Up</Link>
+        </span>
+      </div>
+    </div>
   )
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -6,9 +6,9 @@ export const SignUp = () => {
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl text-center my-7 font-semibold'>Sign Up</h1>
       <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' />
-        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' />
+        <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username' />
+        <input type="email" placeholder='Email' className='border p-3 rounded-lg' id='email' />
+        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' />
         <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">Sign Up</button>
       </form>
       <div className="flex gap-2 mt-5">
