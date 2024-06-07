483458952105f1df557ace2ecc7a80812c72125c
ShauryaA <shauryaanand9941@gmail.com>
2024-06-07 14:34:25 +0530

    Created the update user API route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import dotenv from 'dotenv';
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -15,9 +16,12 @@ mongoose.connect(process.env.MONGO).then(()=>{
 });
 
 const app = express();
+
 app.use(cors());
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
     connectToMongoDB();
