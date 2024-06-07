dfb8ae3e5ef1c4172450e67a65466615329b0b21
ShauryaA <shauryaanand9941@gmail.com>
2024-06-08 02:39:31 +0530

    Completed the Update user functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,71 +1,85 @@
-// import React from 'react'
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
-import { OAuth } from "../components/OAuth";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import {OAuth} from '../components/OAuth';
 
 export const SignUp = () => {
-
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch('http://localhost:3000/api/auth/signup', {
+      const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-  
       console.log(data);
-      if(data.success === false) {
+      if (data.success === false) {
         setLoading(false);
         setError(data.message);
         return;
       }
       setLoading(false);
       setError(null);
-      navigate('/sign-in')
+      navigate('/sign-in');
     } catch (error) {
       setLoading(false);
       setError(error.message);
     }
   };
-
-  console.log(formData);
-
   return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className='text-3xl text-center my-7 font-semibold'>Sign Up</h1>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username' onChange={handleChange} />
-        <input type="email" placeholder='Email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
-        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
-        <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+        <input
+          type='text'
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+          onChange={handleChange}
+        />
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
+
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
-        <OAuth />
+        <OAuth/>
       </form>
-      <div className="flex gap-2 mt-5">
-        <p>Already have an account?</p>
-        <Link to="/sign-in">
-          <span className="text-blue-700">Sign In</span>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to={'/sign-in'}>
+          <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
-      {error && <p className="text-red-500 mt-5">{error}</p>}
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-  )
+  );
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,10 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     currentUser: null,
     error: null,
     loading: false,
-}
+};
 
 const userSlice = createSlice({
     name: 'user',
@@ -21,10 +21,22 @@ const userSlice = createSlice({
         signInFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
+        },
+        updateUserStart: (state) => {
+            state.loading = true;
+        },
+        updateUserSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        updateUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
         }
     }
 });
 
-export const {signInStart, signInSuccess, signInFailure} = userSlice.actions;
+export const { signInStart, signInSuccess, signInFailure, updateUserStart, updateUserSuccess, updateUserFailure } = userSlice.actions;
 
-export default userSlice.reducer; // we are reporting it as default, therefore we can change it's name wherever we are importing it.
+export default userSlice.reducer;
