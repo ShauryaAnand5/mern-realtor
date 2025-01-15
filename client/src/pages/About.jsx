33734867cccbd4725488578b219cafd89a168191
ShauryaA <shauryaanand9941@gmail.com>
2025-01-15 18:55:50 +0530

    Completed the About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,11 @@
 
 export const About = () => {
   return (
-    <div>About</div>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Shaurya Estate</h1>
+      <p className="mb-4 text-slate-700">Welcome to Shaurya Estate, your trusted partner in buying, selling, and renting real estate. With a passion for helping clients find their perfect home or investment, we offer a comprehensive selection of properties that cater to all needs and budgets.</p>
+      <p className="mb-4 text-slate-700">Whether you're a first-time homebuyer, looking to rent, or seeking to sell or invest, our experienced team is here to guide you every step of the way. We pride ourselves on providing exceptional customer service, in-depth market knowledge, and innovative solutions to ensure a seamless experience.</p>
+      <p className="mb-4 text-slate-700">At Shaurya Estate, we understand that real estate is more than just a transaction—it’s about building lasting relationships and making your dreams a reality. Let us help you find your next home, investment property, or rental. Explore our listings today and discover the possibilities.</p>
+    </div>
   )
 }
